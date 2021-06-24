6727c869d24a391a4ebebf54d396021f707e5f77
danielmeller <dnl.meller@gmail.com>
2021-06-24 20:47:51 +0300

    add cors

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 import flask
 from flask import jsonify , request
 from DBWrapper import DBWrapper
+from flask_cors import CORS, cross_origin
 app = flask.Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["DEBUG"] = True
 db = DBWrapper()
 @app.route('/getCampigns', methods=['GET'])
